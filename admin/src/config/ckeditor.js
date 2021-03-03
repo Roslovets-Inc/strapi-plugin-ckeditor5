3570019de83dbb7d933dd215f24f230f540b2400
Pavel Roslovets <roslovets@exponenta.ru>
2021-03-03 11:10:26 +0300

    Update config template

--- admin/src/config/ckeditor.js
+++ admin/src/config/ckeditor.js
@@ -12,6 +12,7 @@ module.exports = ({
         //     'bold',
         //     'italic',
         //     'underline',
+        //     'code',
         //     'link',
         //     'bulletedList',
         //     'numberedList',
@@ -32,7 +33,7 @@ module.exports = ({
         //     '|',
         //     'fullScreen',
         //     'undo',
-        //     'redo'
+        //     'redo',
         // ],
         // shouldNotGroupWhenFull: true
     },
@@ -40,7 +41,7 @@ module.exports = ({
         // styles: [
         //     'alignLeft',
         //     'alignCenter',
-        //     'alignRight'
+        //     'alignRight',
         // ],
         // resizeOptions: [
         //     {
@@ -70,14 +71,16 @@ module.exports = ({
         //     'imageResize:75',
         //     'imageResize:original',
         //     '|',
-        //     'linkImage'
+        //     'linkImage',
         // ]
     },
     table: {
         // contentToolbar: [
         //     'tableColumn',
         //     'tableRow',
-        //     'mergeTableCells'
+        //     'mergeTableCells',
+        //     'tableProperties',
+        //     'tableCellProperties',
         // ]
     },
     heading: {
@@ -86,7 +89,7 @@ module.exports = ({
         //     { model: 'heading1', view: 'h1', title: 'Heading 1', class: 'ck-heading_heading1' },
         //     { model: 'heading2', view: 'h2', title: 'Heading 2', class: 'ck-heading_heading2' },
         //     { model: 'heading3', view: 'h3', title: 'Heading 3', class: 'ck-heading_heading3' },
-        //     { model: 'heading4', view: 'h4', title: 'Heading 4', class: 'ck-heading_heading4' }
+        //     { model: 'heading4', view: 'h4', title: 'Heading 4', class: 'ck-heading_heading4' },
         // ]
     },
     htmlEmbed: {
