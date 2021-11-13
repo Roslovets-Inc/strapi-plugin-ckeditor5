a3efe657bdc6ea274754fa3c48b502e8b8062733
Pavel Roslovets <p.v.roslovets@gmail.com>
2021-11-13 04:27:59 +0300

    chore: add list of font colors to config

--- admin/src/config/ckeditor.js
+++ admin/src/config/ckeditor.js
@@ -88,6 +88,25 @@ module.exports = {
     //     "tableCellProperties",
     // ]
   },
+  fontColor: {
+    // colors: [
+    //   { color: "hsl(0, 0%, 0%)", label: "Black" },
+    //   { color: "hsl(0, 0%, 30%)", label: "Dim grey" },
+    //   { color: "hsl(0, 0%, 60%)", label: "Grey" },
+    //   { color: "hsl(0, 0%, 90%)", label: "Light grey" },
+    //   { color: "hsl(0, 0%, 100%)", label: "White", hasBorder: true },
+    //   { color: "hsl(0, 75%, 60%)", label: "Red" },
+    //   { color: "hsl(30, 75%, 60%)", label: "Orange" },
+    //   { color: "hsl(60, 75%, 60%)", label: "Yellow" },
+    //   { color: "hsl(90, 75%, 60%)", label: "Light green" },
+    //   { color: "hsl(120, 75%, 60%)", label: "Green" },
+    //   { color: "hsl(150, 75%, 60%)", label: "Aquamarine" },
+    //   { color: "hsl(180, 75%, 60%)", label: "Turquoise" },
+    //   { color: "hsl(210, 75%, 60%)", label: "Light blue" },
+    //   { color: "hsl(240, 75%, 60%)", label: "Blue" },
+    //   { color: "hsl(270, 75%, 60%)", label: "Purple" },
+    // ],
+  },
   heading: {
     // options: [
     //     { model: "paragraph", title: "Paragraph", class: "ck-heading_paragraph" },
@@ -96,6 +115,52 @@ module.exports = {
     //     { model: "heading3", view: "h3", title: "Heading 3", class: "ck-heading_heading3" },
     //     { model: "heading4", view: "h4", title: "Heading 4", class: "ck-heading_heading4" },
     // ]
+  },
+  highLight: {
+    // options: [
+    //   {
+    //     model: "yellowMarker",
+    //     class: "marker-yellow",
+    //     title: "Yellow marker",
+    //     color: "var(--ck-highlight-marker-yellow)",
+    //     type: "marker",
+    //   },
+    //   {
+    //     model: "greenMarker",
+    //     class: "marker-green",
+    //     title: "Green marker",
+    //     color: "var(--ck-highlight-marker-green)",
+    //     type: "marker",
+    //   },
+    //   {
+    //     model: "pinkMarker",
+    //     class: "marker-pink",
+    //     title: "Pink marker",
+    //     color: "var(--ck-highlight-marker-pink)",
+    //     type: "marker",
+    //   },
+    //   {
+    //     model: "blueMarker",
+    //     class: "marker-blue",
+    //     title: "Blue marker",
+    //     color: "var(--ck-highlight-marker-blue)",
+    //     type: "marker",
+    //   },
+    //   {
+    //     model: "redPen",
+    //     class: "pen-red",
+    //     title: "Red pen",
+    //     color: "var(--ck-highlight-pen-red)",
+    //     type: "pen",
+    //   },
+    //   {
+    //     model: "greenPen",
+    //     class: "pen-green",
+    //     title: "Green pen",
+    //     color: "var(--ck-highlight-pen-green)",
+    //     type: "pen",
+    //   },
+    // ],
   },
   htmlEmbed: {
     // showPreviews: true,
