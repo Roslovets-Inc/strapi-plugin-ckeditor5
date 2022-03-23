7354372fd219e98ac188c31ac0b06f8368af8f2f
Pavel Roslovets <p.v.roslovets@gmail.com>
2022-03-24 02:14:41 +0300

    fix: fontSize visualization

--- admin/src/components/CKEditor/index.js
+++ admin/src/components/CKEditor/index.js
@@ -17,7 +17,7 @@ const Wrapper = styled.div`
       line-height: initial;
     }
     /* Revert font styling */
-    div, span, applet, object, iframe, h1, h2, h3, h4, h5, h6, p, blockquote, pre, a, abbr, acronym, address, big, cite, code, del, dfn, em, img, ins, kbd, q, s, samp, small, strike, strong, sub, sup, tt, var, b, u, i, center, dl, dt, dd, ol, ul, li, fieldset, form, label, legend, table, caption, tbody, tfoot, thead, tr, th, td, article, aside, canvas, details, embed, figure, figcaption, footer, header, hgroup, menu, nav, output, ruby, section, summary, time, mark, audio, video {
+    div, applet, object, iframe, h1, h2, h3, h4, h5, h6, p, blockquote, pre, a, abbr, acronym, address, big, cite, code, del, dfn, em, img, ins, kbd, q, s, samp, small, strike, strong, sub, sup, tt, var, b, u, i, center, dl, dt, dd, ol, ul, li, fieldset, form, label, legend, table, caption, tbody, tfoot, thead, tr, th, td, article, aside, canvas, details, embed, figure, figcaption, footer, header, hgroup, menu, nav, output, ruby, section, summary, time, mark, audio, video {
       font: revert;
     }
     /* Revert list styling */
